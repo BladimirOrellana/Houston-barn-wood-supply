55c06688a7d0b1cf05c39887ab4d526951a89ba1
Bladimir Orellana <me@bladimirorellana.com>
2024-11-19 13:55:51 -0600

    k

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ app.use(cors());
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
+
+// Your routes here
+app.use(routes);
+// Serve static files from the React app
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Your routes here
-app.use(routes);
+// Catch-all handler for any request that doesn't match
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 // Connect to MongoDB
 connectDB();
 const PORT = process.env.PORT || 5000;
